f0f63081f3993ccee81eba37b95a92bfb7ed711b
DianaIannoniPolaquini <108705643+DianaIannoniPolaquini@users.noreply.github.com>
2022-07-14 16:37:15 -0300

    primeiro commit

---
+++ COMPRAS_DOLAR/ConversorDeMoeda.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace COMPRAS_DOLAR
+{
+    internal class ConversorDeMoeda
+    {
+        
+        public static double ValorReais (double x, double y)
+        {
+            double total = x * y;
+            return total + total * 0.06;
+        }
+
+        
+    }
+}
---
+++ COMPRAS_DOLAR/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace COMPRAS_DOLAR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual a cotação do dolar? ");
+            double x = double.Parse(Console.ReadLine());
+            Console.Write("Quantos dólares você irá comprar? ");
+            double y = double.Parse(Console.ReadLine());
+           
+            double result = ConversorDeMoeda.ValorReais(x, y);
+            
+            Console.WriteLine($"Valor a ser pago em reais = {result} reais.");
+        }
+    }
+}
